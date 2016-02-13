68ecbbb7f0e0c7ee374b5ec06f471d5e64bc410b
Shandy-ko <shandy1124gaff@gmail.com>
2016-02-14 00:14:00 +0900

    3

--- main.rb
+++ main.rb
@@ -1,65 +1,41 @@
 # coding: utf-8
 require './twcon.rb'
-
 # eveの初期化
-eve = Eve.new
+eve=Eve.new
 
 
-
-reply = ['そうだ','へえ!','ここだよ','さよなら','そうだよ',
-         '困ったね','そのとおり','お幸せに','なにかさがしてるの?',
-         'まったくもってわからない','いわれてるから','うん','どうして?',
-         'そうだね……','ここでなにしてるの?','きみをひとりにはしない',
-         'ちゃんとしてるんだ、わたしは','どういうこと?','えっ?','むむむ',
-         'こんばんは','さようなら','こんにちは','アハハハハハ','ハハハハハハ',
-         'アッハッハッハッハッ','何が胎児をそうさせたか','あっ','ハッハッハッハッ',
-         '……う……うん……','……………………','夢','ふーむ','ハッハッハッ',
-         'ハハハハハ','ハイ','うん','いや','胎児の夢','……………',
-         'いいえ','これだわ','いやどうして','そうでしょう','ところがねえ',
-         'そうなるかしら','ようくわかりました','それは、またなぜにです？',
-         '明察です','なるほど……','そうなんだわ','そうですか','悪いことをしたわ',
-         'そうです','なるほど','冗談じゃない','ええ','ありがとう','ばかばかしいわ',
-         'ごめんなさい','それで?','その次は？'
-        ]
-
-called_name = ['なに?', '呼んだ?','どうしたの?']
+reply=YAML.load_file('./reply.yml')
+called_name=['なに?', '呼んだ?','どうしたの?']
 
 # timelineの監視
 begin
-  eve.timeline.userstream do |status|
-    speak = Speak.new
-    contents = status.text
-    next if contents =~ /^RT/
+  eve.timeline.userstream{|status|
+    contents=status.text
+    next if(contents=~/^RT/)
 
-    id = status.user.screen_name
-    speak.user = id
-    id = '@' + id + ' '
+    id=status.user.screen_name
+    id='@' + id + ' '
 
-    speak.id = status.id
-    speak.in_reply_to_status_id = status.in_reply_to_status_id
-    speak.created_at = status.created_at
-    speak.words = contents
-    speak.save
-
-    if contents =~ /@lapis_ko/
+    if(contents=~/@lapis_ko/)
       word = reply.sample
       eve.say(id + word, status.id)
       next
     end
 
     # メンション以外の名前に反応
-    if contents =~ /eve|Eve|イヴ|いぶ/ && status.user.screen_name != '_eve'
-      word = called_name.sample
-      eve.say(id + word, status.id)
+    if(contents=~/eve|Eve|イヴ|いぶ/)&&(status.user.screen_name!='_eve')
+      word=called_name.sample
+      eve.say(id+word, status.id)
       next
     end
 
-    # nzwがつぶやいたらリプライ
-    if status.user.screen_name == 'lapis_zero09'
-      eve.say(id + reply.sample, status.id)
+    # shandyがつぶやいたらリプライ
+    if(status.user.screen_name=='lapis_zero09')
+      replap=["勉強しんさい","Twitterやめんさい"]
+      eve.say(id + replap.sample, status.id)
       next
     end
-  end
+  }
 
 rescue => e
   puts e.message
--- twcon.rb
+++ twcon.rb
@@ -7,28 +7,27 @@ require 'tweetstream'
 class Eve
   # 外部から参照できるメンバ変数
   attr_accessor :client, :timeline
+
   def initialize
-    keys = YAML.load_file('./config.yml')
+    keys=YAML.load_file('./config.yml')
 
     # restAPI初期化
-    @client = Twitter::REST::Client.new do |config|
-      config.consumer_key = keys["api_key"]
-      config.consumer_secret = keys["api_secret"]
-      config.access_token = keys["access_token"]
-      config.access_token_secret = keys["access_token_secret"]
-    end
-
+    @client=Twitter::REST::Client.new{|config|
+      config.consumer_key=keys["api_key"]
+      config.consumer_secret=keys["api_secret"]
+      config.access_token=keys["access_token"]
+      config.access_token_secret=keys["access_token_secret"]
+    }
 
     # StreamAPI初期化
-    TweetStream.configure do |config|
-      config.consumer_key = keys["api_key"]
-      config.consumer_secret = keys["api_secret"]
-      config.oauth_token = keys["access_token"]
-      config.oauth_token_secret = keys["access_token_secret"]
-      config.auth_method = :oauth
-    end
-
-    @timeline = TweetStream::Client.new
+    TweetStream.configure{|config|
+      config.consumer_key=keys["api_key"]
+      config.consumer_secret=keys["api_secret"]
+      config.oauth_token=keys["access_token"]
+      config.oauth_token_secret=keys["access_token_secret"]
+      config.auth_method=:oauth
+    }
+    @timeline=TweetStream::Client.new
   end
 
   # 引数を投稿するメソッド
